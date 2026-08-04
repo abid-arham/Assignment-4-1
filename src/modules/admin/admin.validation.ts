1bca23105dc97c5c777cd7dfdf1a67bf427e00fa
abid-arham <abid-arham@users.noreply.github.com>
2026-08-04 19:32:04 +0600

    feat: add validation schemas for admin and user modules

--- src/modules/admin/admin.validation.ts
+++ src/modules/admin/admin.validation.ts
@@ -1,1 +1,10 @@
-export {};
+import { z } from "zod"
+import { ActiveStatus } from "../../../generated/prisma/enums"
+
+const updateUserStatus = z.object({
+  body: z.object({
+    activeStatus: z.nativeEnum(ActiveStatus)
+  })
+})
+
+export const adminValidation = { updateUserStatus }
--- src/modules/users/user.validation.ts
+++ src/modules/users/user.validation.ts
@@ -1,1 +1,10 @@
-export {};
+import { z } from "zod"
+
+const updateProfile = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    phone: z.string().optional()
+  })
+})
+
+export const userValidation = { updateProfile }
